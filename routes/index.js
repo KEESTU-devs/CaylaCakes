0b93f8632e46fbf55fbc38311f2f3b60ff428597
Keenan Sturtevant <keenan.sturtevant@childrens.com>
2018-10-05 14:45:40 -0500

    update shopping cart

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index')
+  const shoppingCart = [
+    {
+      name: 'Flake Cookies',
+      quantity: 1,
+      unitPrice: 4.99
+    },
+    {
+      name: 'Sugar Cookies',
+      quantity: 1,
+      unitPrice: 2.99
+    }
+  ]
+
+  let total = 0
+  let tax
+
+  shoppingCart.map(item => total = total + item.unitPrice)
+  tax = Math.round((total * 0.06) * 100) / 100
+  res.render('index', { shoppingCart, tax, total })
 })
 
 module.exports = router
