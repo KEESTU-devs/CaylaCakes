08340d15bb8d14a16c6c7a3341a50dc360847fbb
Keenan Sturtevant <keenan.sturtevant@childrens.com>
2018-10-07 04:06:51 -0500

    finishes ajax adding to cart

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,26 @@ function generateCartHtml (cart) {
   return response
 }
 
+function generateTotals (cart) {
+  let total = 0
+  let tax
+
+  cart.forEach(item => total = total + parseFloat(item.unitPrice))
+  tax = (total * 0.06).toFixed(2)
+  total = total.toFixed(2)
+
+   return `<div class="row">
+      <small>6% tax</small> 
+      <span> $${tax}</span>
+      <div class="row">
+         <hr>
+         <div class="row">
+           <p>Order Total: $${total}</p>
+         </div>
+      </div>
+    </div>`
+}
+
 function rootAction (req, res) {
   try {
     let cart = JSON.parse(req.cookies['shopping-cart']) || []
@@ -36,7 +56,7 @@ function emptyCart (req, res) {
   try {
     const cart = []
     res.cookie('shopping-cart', JSON.stringify(cart))
-    res.status(200).send({ cart: generateCartHtml(cart) })
+    res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
@@ -48,7 +68,7 @@ function removeItem (req, res) {
     let cart = JSON.parse(req.cookies['shopping-cart'])
     cart = cart.filter(item => item.name !== req.body.item)
     res.cookie('shopping-cart', JSON.stringify(cart))
-    res.status(200).send({ cart: generateCartHtml(cart) })
+    res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
@@ -61,7 +81,7 @@ function addToCart (req, res) {
     let cart = JSON.parse(req.cookies['shopping-cart']) || []
     cart = [...cart, ...item]
     res.cookie('shopping-cart', JSON.stringify(cart))
-    res.status(200).send({ cart: generateCartHtml(cart) })
+    res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
