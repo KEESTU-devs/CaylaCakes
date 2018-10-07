673424b917ff73e346edaf17d009f046bca3dd39
Keenan Sturtevant <keenan.sturtevant@childrens.com>
2018-10-07 03:00:40 -0500

    update shopping cart logic

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,42 @@
-const express = require('express')
+const express = require('express'), cookieParser = require('cookie-parser')
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const shoppingCart = [
-    {
-      name: 'Flake Cookies',
-      quantity: 1,
-      unitPrice: 4.99
-    },
-    {
-      name: 'Sugar Cookies',
-      quantity: 1,
-      unitPrice: 2.99
-    }
-  ]
+  let cart = JSON.parse(req.cookies['shopping-cart']) || []
 
   let total = 0
   let tax
 
-  shoppingCart.map(item => total = total + item.unitPrice)
-  tax = Math.round((total * 0.06) * 100) / 100
-  res.render('index', { shoppingCart, tax, total })
+  cart.forEach(item => total = total + parseFloat(item.unitPrice))
+  tax = (total * 0.06).toFixed(2)
+  total = total.toFixed(2)
+
+  res.render('index', { cart, tax, total })
+})
+
+router.post('/clearCart', function (req, res, next) {
+  res.cookie('shopping-cart', JSON.stringify([]))
+  res.sendStatus(200)
+})
+
+router.post('/removeItem', function (req, res, next) {
+  let cart = JSON.parse(req.cookies['shopping-cart'])
+  cart = cart.filter(item => item.name !== req.body.item)
+  res.cookie('shopping-cart', JSON.stringify(cart))
+  res.sendStatus(200)
+})
+
+router.post('/addToCart', function (req, res, next) {
+  try {
+    const item = [{ name: req.body.name, quantity: req.body.quantity, unitPrice: req.body.unitPrice }]
+    let cart = JSON.parse(req.cookies['shopping-cart']) || []
+    cart = [...cart, ...item]
+    res.cookie('shopping-cart', JSON.stringify(cart))
+    res.sendStatus(200)
+  } catch {
+    res.sendStatus(500)
+  }
 })
 
 module.exports = router
