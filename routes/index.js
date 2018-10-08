0a89fb944955268af8be9de482667c23613b61f2
Keenan Sturtevant <keenan.sturtevant@childrens.com>
2018-10-08 18:43:55 -0500

    add google analytics

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+const ua = require('universal-analytics')
+const visitor = ua(process.env.GOOGLE_ANALYTICS_ID, { http: true })
+
+function trackEvent (page, title) {
+  try {
+    visitor.pageview(page, 'caylacakes.com', title).send()
+  } catch (error) {
+    console.log('Google Analytics Error: ', error)
+  }
+}
+
+function trackError (page, title) {
+  try {
+    const visitor = ua(process.env.GOOGLE_ANALYTICS_ID, { http: true })
+    visitor.exception(page, 'caylacakes.com', title).send()
+  } catch (error) {
+    console.log('Google Analytics Error: ', error)
+  }
+}
+
 function generateCartHtml (cart) {
   let response = ''
   cart.forEach(item => {
@@ -45,9 +65,11 @@ function rootAction (req, res) {
     tax = (total * 0.06).toFixed(2)
     total = total.toFixed(2)
 
+    trackEvent('/', 'index')
     res.render('index', { cart, tax, total })
   } catch (error) {
     console.log(error)
+    trackError('/', 'index')
     res.sendStatus(500)
   }
 }
@@ -56,9 +78,11 @@ function emptyCart (req, res) {
   try {
     const cart = []
     res.cookie('shopping-cart', JSON.stringify(cart))
+    trackEvent('/clearCart', 'empty-cart')
     res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
+    trackError('/clearCart', 'empty-cart')
     res.sendStatus(500)
   }
 }
@@ -68,9 +92,11 @@ function removeItem (req, res) {
     let cart = JSON.parse(req.cookies['shopping-cart'])
     cart = cart.filter(item => item.name !== req.body.item)
     res.cookie('shopping-cart', JSON.stringify(cart))
+    trackEvent('/removeItem', 'remove-item')
     res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
+    trackError('/removeItem', 'remove-item')
     res.sendStatus(500)
   }
 }
@@ -81,9 +107,11 @@ function addToCart (req, res) {
     let cart = req.cookies['shopping-cart'] ? JSON.parse(req.cookies['shopping-cart']) : []
     cart = [...cart, ...item]
     res.cookie('shopping-cart', JSON.stringify(cart))
+    trackEvent('/addToCart', 'add-to-cart')
     res.status(200).send({ cart: generateCartHtml(cart), totals: generateTotals(cart) })
   } catch (error) {
     console.log(error)
+    trackError('/addToCart', 'add-to-cart')
     res.sendStatus(500)
   }
 }
